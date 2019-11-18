bfba42eb733a5b756dce44920aa16d73b2f9ee37
sanjulapro <desanjula99@gmail.com>
2019-11-18 20:55:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import Search from './components/users/Search';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component{
 
+  state = {
+    users: [],
+    loading: false,
+    alert: null,
+  };
+
+  //search github users
+  searchUsers = async text => {
+    const res = await axios.get(
+      `https://api.github.com/users?q=${text}&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      );
+
+    this.setState({ users:res.data, loading: false });
+  };
+
+  // Clear users from state
+
+  clearUsers = () => this.setState({ users: [], loading: false });
+
+  // Set Alert
+  setAlert = (msg, type) => {
+     this.setState({ alert: { msg, type } });
+  };
+
+  render() {
+    const { users,loading } = this.state;
+
+    return (
+      <div className= 'App'>
+        <Navbar />
+        <div className='container'>
+        <Search searchUsers={this.searchUsers}
+                clearUsers={this.clearUsers}
+                showClear={users.length > 0 ? true : false}
+                 />
+        <Users loading={loading} users={users}/>
+        </div>
+      </div>
+    );
+  }
+}
 export default App;
